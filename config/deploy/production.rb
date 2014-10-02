755c6c430cbd24003845a076fd33e9eba632dd0e
Prasanna <prasofty@gmail.com>
2014-10-02 12:10:36 +0530

    second commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,22 +1,16 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+set :stage, :production
+set :branch, :master
+set :rails_env, :production
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
-
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
-
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
+server '50.116.21.162',
+       ssh_options: {
+         user: 'root',
+         forward_agent: false,
+         auth_methods: %w(password publickey),
+         password: ENV['PRODUCTION_SERVER_PASSWORD'],
+         keys: %w(~/.ssh/id_rsa.pub)
+       },
+       roles: [:web, :app, :db]
 
 # Custom SSH Options
 # ==================
